7c638ca5e9967ca9f946ed3f26a6d98905df2230
luojianrong <luojianrong00@gmail.com>
2019-07-12 19:58:20 +0800

    完善了基本全部功能

--- js/index.js
+++ js/index.js
@@ -8,7 +8,12 @@ window.onload = function () {
     var homePoint = document.querySelectorAll(".home-point li");
     var homeCarousel = document.querySelectorAll(".home-carousel li");
     var home = document.querySelector(".home");
-
+    var teamLists = document.querySelectorAll(".team-lists li");
+    var teamUl = document.querySelector(".team-lists");
+    var canvas = null;
+    var headerMusic = document.querySelector(".header-main-music");
+    var audio = document.querySelector("audio");
+    var contentPoint = document.querySelectorAll(".content-point li");
 
     //定义变量
     var arrowWidth = headerArrow.width / 2;//移动小图标的宽度/2
@@ -19,6 +24,7 @@ window.onload = function () {
     var lastIndex = 0;//保存上一次点击的小图标下标
     var timer = null;//防抖定时器
     var lastTime = 0;//节流，保存上一次触发的时间
+    var lastAnimation = 0;
 
     setLeft(headerNavLi[0]);
     //封装函数，改变移动小图标的left值
@@ -95,7 +101,6 @@ window.onload = function () {
 
     }
 
-    move(1);
     //封装函数，鼠标滚动时，设置ul的位置，设置li的width，设置arrow的位置
     function move(index){
         // 去除第一个active
@@ -108,8 +113,16 @@ window.onload = function () {
         for (var j = 0, length = headBottom.length; j < length; j++) {
             var headerNavBottomElement = headBottom[j];
             headerNavBottomElement.style.width = '';
+
+            contentPoint[j].className = "";
         }
+        animationArr[index].animationIn();
+        animationArr[lastAnimation].animationOut();
+
+        lastAnimation = index;
+
         headBottom[index].style.width = '100%';
+        contentPoint[index].className = "active";
 
         setLeft(headerNavLi[index]);
     }
@@ -151,6 +164,7 @@ window.onload = function () {
     autoMove();
     function autoMove(){
         autoTime = setInterval(function () {
+            animationArr[0].animationIn();
             var nowIndex = lastIndex+1;
             if (nowIndex>3)  nowIndex = 0;
             //切换页面
@@ -170,6 +184,212 @@ window.onload = function () {
     //鼠标移出事件
     home.onmouseleave = function () {
         autoMove();
+    }
+
+   //第五屏
+   //鼠标移入事件
+    for (var i = 0; i <teamLists.length ; i++) {
+        teamLists[i].index = i;
+        teamLists[i].onmouseenter = function(){
+            for (var i = 0; i <teamLists.length ; i++) {
+                teamLists[i].style.opacity = 0.5;
+            }
+            teamLists[this.index].style.opacity = 1;
+            bubbleMotion(this.index);
+        }
+    }
+    //鼠标移出事件
+    teamUl.onmouseleave = function () {
+        canvas.remove();
+        canvas = null;
+        for (var i = 0; i <teamLists.length ; i++) {
+            teamLists[i].style.opacity = 1;
+        }
+    }
+
+    //气泡
+    function bubbleMotion(index) {
+        if (!canvas) {
+            canvas = document.createElement("canvas");
+            canvas.className = "myCanvas";
+            canvas.width = 236;
+            canvas.height = 448;
+            canvas.style.top = 0;
+            canvas.style.left = index*236 +"px";
+
+            if (canvas.getContext) {
+
+                var arr = [];
+                var ctx = canvas.getContext('2d');
+
+                // 画圆
+                setInterval(function () {
+
+                    // 清除画布
+                    ctx.clearRect(0, 0, 236, 448);
+
+                    for (var i = 0; i < arr.length; i++) {
+                        var item = arr[i];
+
+                        // 产生正弦运动
+                        // 角度递增 --> 决定运动的速度
+                        item.deg += 2;
+                        // 根据角度求弧度
+                        var rad = item.deg * Math.PI / 180;
+                        // 求得横坐标和纵坐标的值
+                        var x = item.x + item.scale * Math.sin(rad);
+                        var y = item.y - item.scale * rad;
+
+                        ctx.fillStyle = `rgb(${item.r}, ${item.g}, ${item.b})`;
+                        ctx.beginPath();
+                        ctx.arc(x, y, item.R, 0, 2 * Math.PI);
+                        ctx.fill();
+
+                    }
+
+                }, 1000 / 60);
+
+                // 产生圆
+                setInterval(function () {
+                    // 随机颜色
+                    var r = Math.round(Math.random() * 255);
+                    var g = Math.round(Math.random() * 255);
+                    var b = Math.round(Math.random() * 255);
+                    // 随机半径
+                    var R = Math.round(Math.random() * 8  + 2);
+                    // 随机初始位置
+                    var y = 400 + R;
+                    var x = Math.round(Math.random() * 150);
+                    // 随机缩放系数
+                    var scale = Math.round(Math.random() * 50  + 50);
+
+                    // 添加到数组
+                    arr.push({
+                        r: r,
+                        g: g,
+                        b: b,
+                        x: x,
+                        y: y,
+                        R: R,
+                        deg: 0,
+                        scale: scale
+                    });
+
+                }, 100)
+
+                teamUl.appendChild(canvas);
+            }
+        }else{
+            canvas.style.left = index*236 +"px";
+        }
+    }
+
+    /*音乐播放事件*/
+    headerMusic.onclick = function () {
+        if (audio.paused) {
+            // 说明当前音乐暂停状态，需要播放
+            audio.play();
+            this.style.backgroundImage = 'url("./imgs/musicon.gif")';
+        } else {
+            // 说明当前音乐播放状态，需要暂停
+            audio.pause();
+            this.style.backgroundImage = 'url("./imgs/musicoff.gif")';
+        }
+    }
+
+    //侧边栏效果
+    for (var i = 0; i <contentPoint.length ; i++) {
+        contentPoint[i].index = i;
+        contentPoint[i].onclick = function () {
+            index = this.index;
+            move(index);
+        }
+    }
+
+
+    var plane1 = document.querySelector('.plane1');
+    var plane2 = document.querySelector('.plane2');
+    var plane3 = document.querySelector('.plane3');
+
+    var pencil1 = document.querySelector('.pencel1');
+    var pencil2 = document.querySelector('.pencel2');
+    var pencil3 = document.querySelector('.pencel3');
+
+    var home = document.querySelector(".home");
+
+    var photo1 = document.querySelector('.about-photo-first');
+    var photo2 = document.querySelector('.about-photo-second');
+
+    var teamTitle = document.querySelector('.team-title');
+    var teamContent = document.querySelector('.team-content');
+
+    var animationArr = [
+      {
+          //出场动画
+          animationOut: function () {
+              home.style.transform = "translateY(-200px)";
+          },
+          // 入场动画
+          animationIn: function () {
+              home.style.transform = "translateY(0)";
+          }
+      },
+        {
+            //出场动画
+            animationOut: function () {
+                plane1.style.transform = "translate(-200px,-200px)";
+                plane2.style.transform = "translate(-200px,200px)";
+                plane3.style.transform = "translate(200px,-200px)";
+            },
+            // 入场动画
+            animationIn: function () {
+                plane1.style.transform = "translate(0)";
+                plane2.style.transform = "translate(0)";
+                plane3.style.transform = "translate(0)";
+            }
+        },
+        {
+            //出场动画
+            animationOut: function () {
+                pencil1.style.transform = "translateY(-200px)";
+                pencil2.style.transform = "translateY(200px)";
+                pencil3.style.transform = "translateY(200px)";
+            },
+            // 入场动画
+            animationIn: function () {
+                pencil1.style.transform = "translate(0)";
+                pencil2.style.transform = "translate(0)";
+                pencil3.style.transform = "translate(0)";
+            }
+        },
+        {
+            //出场动画
+            animationOut: function () {
+                photo1.style.transform = "rotate(45deg)";
+                photo2.style.transform = "rotate(45deg)";
+            },
+            // 入场动画
+            animationIn: function () {
+                photo1.style.transform = "rotate(0)";
+                photo2.style.transform = "rotate(0)";
+            }
+        },
+        {
+            //出场动画
+            animationOut: function () {
+                teamTitle.style.transform = "translateX(-200px)";
+                teamContent.style.transform = "translateX(200px)";
+            },
+            // 入场动画
+            animationIn: function () {
+                teamTitle.style.transform = "translateX(0)";
+                teamContent.style.transform = "translateX(0)";
+            }
+        }
+    ];
+
+    for (var i = 0; i < animationArr.length; i++) {
+        animationArr[i].animationOut();
     }
 
 }
